019aca8fb94da6143611e6443a6320f97a2e9dd8
Renee Margaret McConahy <nepella@gmail.com>
2022-08-23 13:21:47 -0400

    Use headings instead of Jekyll's page titles.
    My custom plugin doesn't work on GitHub Pages.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
 gem "jekyll", "~> 4.2"
+gem "jekyll-titles-from-headings"
 
 gem "webrick", "~> 1.7", :group => :development
--- _plugins/titleize_override.rb
+++
@@ -1,9 +0,0 @@
-# Source: <https://stackoverflow.com/a/37990280>.
-
-module Jekyll
-  module Utils
-    def titleize_slug(slug)
-      slug.split(/[_-]/).join(' ').capitalize
-    end
-  end
-end
